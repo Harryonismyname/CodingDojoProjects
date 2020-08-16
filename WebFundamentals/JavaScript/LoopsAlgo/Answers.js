443664d77235aeecd5781545bae69ddbaaf2a64b
Harryon <Harryonismyname@users.noreply.github.com>
2020-08-16 11:30:28 -0500

    Renamed Folders and created Loops Algo project

---
+++ WebFundamentals/JavaScript/LoopsAlgo/Answers.js
@@ -0,0 +1,14 @@
+for(var i = 1; i<=20; i++)
+{
+    if (i % 2 != 0) 
+    {
+        console.log(i);
+    }
+}
+var sum = 0
+for(var num =1; num<=5; num++)
+{
+    console.log("Num:"+num)
+    sum += num
+    console.log("Sum: "+sum)
+}
