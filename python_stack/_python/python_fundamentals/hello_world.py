e4e8f8b27e2c3dc44798db0af96312d4afb96f47
Harryon <Harryonismyname@users.noreply.github.com>
2020-08-29 15:10:52 -0500

    Completed 'Hello World' assignment

--- python_stack/_python/python_fundamentals/hello_world.py
+++ python_stack/_python/python_fundamentals/hello_world.py
@@ -2,3 +2,29 @@ x = "Hello Python"
 print(x)
 y = 42
 print(y)
+
+print("Hello World")
+name = "Ben"
+print("Hello", name)
+print("Hello " + name)
+
+name = 42
+print("Hello", name)
+print("Hello "+ str(name))
+# This is how you comment
+foodOne = "Coconut"
+foodTwo = "Pineapple"
+print("I love to eat {} and {}.".format(foodOne, foodTwo))
+print(f"I love to eat {foodOne} and {foodTwo}.")
+print("I love Food".split(' '))
+print("I love Food".upper())
+print("I love Food".lower())
+print("I love Food".lstrip("I"))
+print("I love Food".replace(" ", "_", 1))
+print("I love Food".replace(" ", "_"))
+print("I love Food".rfind("love"))
+print("I love Food".rjust(50, "="))
+print("I love Food".ljust(50, "="))
+print("I love Food".center(50, "="))
+print("I love Food".count("o"))
+print("I love Food".count("a"))
