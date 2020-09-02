02c9930afa1187037340fed04fb5cb626cbe7598
Harryon <Harryonismyname@users.noreply.github.com>
2020-09-02 18:20:38 -0500

    removed arbitrary assignment in Insertion sort

--- python_stack/_python/python_fundamentals/Sort_Methods/InsertSort/InsertSort.py
+++ python_stack/_python/python_fundamentals/Sort_Methods/InsertSort/InsertSort.py
@@ -2,9 +2,8 @@ arr = [6,5,3,1,8,7,2,4,4,56,2,7,7,8,8,9,9,4,354645,6345,63,4,563,456,3546,345,63
 
 def insertionSort(arr):
     for i in range(len(arr)):
-        index = 0
+        index = arr[i]
         if arr[i]<arr[i-1]:
-            index = arr[i]
             for j in range(i,0,-1):
                 if index<arr[j-1]:
                     arr[j],arr[j-1] = arr[j-1],index
