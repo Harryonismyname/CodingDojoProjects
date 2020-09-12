258dda9aab9587a37913d8ec059dbcc452f67b72
Harryon <Harryonismyname@users.noreply.github.com>
2020-09-12 17:19:17 -0500

    created first django project with one app to complete first django project assignment

---
+++ python_stack/django/django_fundamentals/myDjango/bloggy/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+# Create your views here.
+def index(request):
+    return HttpResponse("placeholder to later display a list of all blogs")
+
+def new(request):
+    return HttpResponse("placeholder to display a new for to create a new blog")
+
+def create(request):
+    return redirect("/index")
+
+def show(request, number):
+    return HttpResponse(f"placeholder to display blog number: {number}")
+
+def edit(request, number):
+    return HttpResponse(f"placeholder to edit blog {number}")
+
+def delete(request, number):
+    return redirect("/index")
