a6c6050eb5bf407d3d323f61f50b0adeba2269e8
Ben Lance <Harryonismyname@users.noreply.github.com>
2020-09-17 18:56:13 -0500

    initialized Views and established html

--- python_stack/django/django_fundamentals/GeniProj/RandomWordGenerator/views.py
+++ python_stack/django/django_fundamentals/GeniProj/RandomWordGenerator/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils.crypto import get_random_string
 
 # Create your views here.
+def index (request):
+    request.session['rando_word'] = get_random_string(length=14)
+    request.session['attempt_num']=request.session.get('attempt_num', 0) + 1    
+    return render(request, 'index.html')
