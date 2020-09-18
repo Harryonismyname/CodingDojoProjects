efc28b15b0e0033cbe76f119902ef57fe1a40071
Harryon <Harryonismyname@users.noreply.github.com>
2020-09-17 21:04:03 -0500

    Created Random Word Generator assignment

--- python_stack/django/django_fundamentals/GeniProj/RandomWordGenerator/views.py
+++ python_stack/django/django_fundamentals/GeniProj/RandomWordGenerator/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils.crypto import get_random_string
 
 # Create your views here.
-def index (request):
-    request.session['rando_word'] = get_random_string(length=14)
-    request.session['attempt_num']=request.session.get('attempt_num', 0) + 1    
+def index (request): 
     return render(request, 'index.html')
+
+def submit(request):
+    request.session['rando_word'] = get_random_string(length=14)
+    request.session['attempt_num']=request.session.get('attempt_num', 0) + 1
+    return redirect("/")
+
+def reset(request):
+    request.session['attempt_num'] = 0
+    return redirect("/")
