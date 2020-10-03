8743732722a001de6b284052852bedf463ed6567
Harryon <Harryonismyname@users.noreply.github.com>
2020-10-03 14:22:08 -0500

    Updated Semi-RESTful TV shows to include form validation

--- python_stack/django/django_fullstack/swohsvt/semi_restful_tv_shows/models.py
+++ python_stack/django/django_fullstack/swohsvt/semi_restful_tv_shows/models.py
@@ -1,4 +1,25 @@
+from __future__ import unicode_literals
 from django.db import models
+from datetime import date
+
+class ShowManager(models.Manager):
+    def basic_validator(self, postData, workingObject=None):
+        errors = {}
+        current_date = str(date.today())
+        all_shows= Show.objects.all()
+        if workingObject is not None:
+            for shows in all_shows:
+                if postData['Title'] == shows.title and postData['Title'] != workingObject.title:
+                    errors['unique_title'] = "Title must be unique"
+        if len(postData['Title']) < 2:
+            errors['title'] = "Title must be greater than 2 characters long"
+        if len(postData['Network']) < 3:
+            errors['network'] = "Network must be greater than 3 characters long"
+        if len(postData['Description'])< 10 and len(postData['Description']) > 0:
+            errors['description'] = "Description must be at least 10 characters long or empty"
+        if postData['Release Date'] > current_date or len(postData['Release Date']) == 0:
+            errors['release_date'] = "Release date must be before today"
+        return errors
 
 # Create your models here.
 class Show(models.Model):
@@ -8,3 +29,4 @@ class Show(models.Model):
     description=models.TextField(default="Some Text")
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
+    objects= ShowManager()
--- python_stack/django/django_fullstack/swohsvt/semi_restful_tv_shows/views.py
+++ python_stack/django/django_fullstack/swohsvt/semi_restful_tv_shows/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Show
+
 
 # Create your views here.
 def index(request):
@@ -15,8 +17,14 @@ def new(request):
     return render(request, 'new.html')
 
 def create(request):
-    Show.objects.create(title=request.POST['Title'], network=request.POST['Network'], release_date=request.POST['Release Date'], description=request.POST['Description'])
-    return redirect('/shows')
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for k, v in errors.items():
+            messages.error(request, v)
+        return redirect('/shows/new')
+    else:
+        Show.objects.create(title=request.POST['Title'], network=request.POST['Network'], release_date=request.POST['Release Date'], description=request.POST['Description'])
+        return redirect('/shows')
 
 def about(request, id):
     context={'id': id, 'show':Show.objects.get(id=id)}
@@ -24,19 +32,24 @@ def about(request, id):
 
 def edit(request,id):
     context = {'id': id, 'show':Show.objects.get(id=id)}
-    print(context['show'].release_date)
     release_date = context['show'].release_date
     context['release_date'] = str(release_date)
     return render(request, 'edit.html', context)
 
 def update(request, id):
-    updated_object= Show.objects.get(id=id)
-    updated_object.title=request.POST['Title']
-    updated_object.network=request.POST['Network']
-    updated_object.release_date=request.POST['Release Date']
-    updated_object.description=request.POST['Description']
-    updated_object.save()
-    return redirect('/shows')
+    errors = Show.objects.basic_validator(request.POST, Show.objects.get(id=id))
+    if len(errors)>0:
+        for k, v in errors.items():
+            messages.error(request, v)
+        return redirect(f'/shows/{id}/edit')
+    else:
+        updated_object= Show.objects.get(id=id)
+        updated_object.title=request.POST['Title']
+        updated_object.network=request.POST['Network']
+        updated_object.release_date=request.POST['Release Date']
+        updated_object.description=request.POST['Description']
+        updated_object.save()
+        return redirect(f'/shows/{id}')
 
 def destroy(request, id):
     deleted_object= Show.objects.get(id=id)
